71dc224452d7158160412229db51dbdbb20db5a7
杨茂聪 <yangmaocong@cvte.com>
2024-03-08 14:06:23 +0800

    提交dist

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,14 +3,14 @@ export default {
   external: id => /^(lib0|yjs|y-protocols)/.test(id),
   output: [{
     name: 'y-websocket',
-    file: 'dist/y-websocket.cjs',
-    format: 'cjs',
+    file: 'dist/y-websocket.js',
+    format: 'es',
     sourcemap: true,
     paths: path => {
       if (/^lib0\//.test(path)) {
         return `lib0/dist${path.slice(4)}.cjs`
       } else if (/^y-protocols\//.test(path)) {
-        return `y-protocols/dist${path.slice(11)}.cjs`
+        return `y-protocols/dist${path.slice(11)}.js`
       }
       return path
     }
